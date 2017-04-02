ab2beaa77a57c899bcdd9059e0436673db113e35
yu.yan <yu.yan@pingxx.com>
2017-04-02 15:18:48 +0800

    continue

--- acheat/__init__.py
+++ acheat/__init__.py
@@ -3,3 +3,4 @@
 from random_feature_selector import random_select
 from gready_feature_selector import gready_select
 from force_feature_selector import force_select
+from random_coff_adjustor import random_adjust
---
+++ acheat/random_coff_adjustor.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+#author=godpgf
+import random
+
+#coff_num_list保存的是每个参数的长度
+#max_loop_time表示随机随机测试多少次找不到最大值就停止
+def random_adjust(evl, coff_num_list, max_loop_time = 32):
+    last_adj_index = -1
+    cur_adj_index = -1
+    best_evl = 0
+    adjust_list = [0 for i in range(len(coff_num_list))]
+    remain_loop_time = max_loop_time
+    while remain_loop_time > 0:
+        --remain_loop_time
+        while cur_adj_index == last_adj_index:
+            cur_adj_index = random.randint(0, len(coff_num_list)-1)
+        last_adj_index = cur_adj_index
+        best_coff_id = adjust_list[cur_adj_index]
+        cur_coff_id = best_coff_id
+        for i in range(coff_num_list[cur_adj_index]):
+            adjust_list[cur_adj_index] = i
+            cur_evl = evl(adjust_list)
+            if cur_evl > best_evl:
+                best_evl = cur_evl
+                best_coff_id = i
+        if best_coff_id != cur_coff_id:
+            adjust_list[cur_adj_index] = best_coff_id
+            remain_loop_time = max_loop_time
+    return adjust_list, best_evl
