34836b38a72b9a736be6b6bcac5ec1d1fc0cba2b
yu.yan <yu.yan@pingxx.com>
2017-11-26 14:48:54 +0800

    continue

--- acheat/random_coff_adjustor.py
+++ acheat/random_coff_adjustor.py
@@ -7,17 +7,20 @@ import random
 def random_adjust(evl, coff_num_list, max_loop_time = 32):
 
     if len(coff_num_list) == 0:
-        return [],eval([])
+        return [],evl([])
 
     last_adj_index = -1
     cur_adj_index = -1
-    best_evl = 0
-    adjust_list = [random.randint(0,len(coff_num_list[i])-1) for i in range(len(coff_num_list))]
+    best_evl = -1
+    adjust_list = [0 for i in range(len(coff_num_list))]
     remain_loop_time = max_loop_time
     while remain_loop_time > 0:
-        --remain_loop_time
+        remain_loop_time -= 1
         while cur_adj_index == last_adj_index:
-            cur_adj_index = random.randint(0, len(coff_num_list)-1) if len(coff_num_list) > 1 else 0
+            if len(coff_num_list) == 1:
+                cur_adj_index = 0
+            else:
+                cur_adj_index = random.randint(0,len(coff_num_list)-1)
         last_adj_index = cur_adj_index
         best_coff_id = adjust_list[cur_adj_index]
         cur_coff_id = best_coff_id
@@ -30,4 +33,6 @@ def random_adjust(evl, coff_num_list, max_loop_time = 32):
         if best_coff_id != cur_coff_id:
             adjust_list[cur_adj_index] = best_coff_id
             remain_loop_time = max_loop_time
+        if len(coff_num_list) == 1:
+            break
     return adjust_list, best_evl
