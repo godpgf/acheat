2a7f0f39da5787f021c8d26fa290658e1cfeb33f
严宇 <yuyan@jieyuechina.com>
2018-07-17 15:23:11 +0800

    comtinue

--- acheat/grid_feature_selector.py
+++ acheat/grid_feature_selector.py
@@ -3,34 +3,37 @@
 import random
 
 
-def grid_select(evl, sel_num, data_num, max_fail_time = 32):
+def grid_select(evl, data_num_list, max_fail_time = 32):
     selection = []
     selection_des = []
     selection_set = set()
-    for i in range(sel_num):
-        selection.append(i)
-        selection_des.append("%d"%i)
+    all_data_cnt = 1
+    for i in range(len(data_num_list)):
+        all_data_cnt *= data_num_list[i]
+        selection.append(0)
+        selection_des.append("%d"%0)
     best_evl = evl(selection, -1, -1)
-    if sel_num == data_num:
+    if all_data_cnt == 1:
         return selection, best_evl
     fail_time = 0
     selection_set.add(','.join(selection_des))
 
     last_replace_index = -1
     while fail_time < max_fail_time:
-        cur_replace_index = random.randint(0, sel_num-1)
+        cur_replace_index = random.randint(0, len(data_num_list)-1)
         while cur_replace_index == last_replace_index:
-            cur_replace_index = random.randint(0, sel_num - 1)
+            cur_replace_index = random.randint(0, len(data_num_list) - 1)
         last_replace_index = cur_replace_index
         last_replace_value = selection[cur_replace_index]
         best_feature_index = -1
-        for i in xrange(data_num):
+        data_num = data_num_list[cur_replace_index]
+        for i in range(data_num):
             if i != last_replace_value and i not in selection:
                 selection[cur_replace_index] = i
 
                 #判断是否出现过
                 sorted_sel = sorted(selection)
-                for j in xrange(sel_num):
+                for j in range(len(data_num_list)):
                     selection_des[j] = "%d"%sorted_sel[j]
                 des = ','.join(selection_des)
                 if des in selection_set:
